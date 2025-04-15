0bd31d653155570885067d85fc65bfa9ced7fd23
maki-exe <maki.toshitaka@gmail.com>
2025-04-15 21:17:49 +0900

    line_bot.py

--- line_bot.py
+++ line_bot.py
@@ -1,19 +1,26 @@
+# line_bot.py
+
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+from dotenv import load_dotenv
+
+# .env 読み込み
+load_dotenv()
 
 app = Flask(__name__)
 
-# あなたのチャネルアクセストークンとシークレット
-LINE_CHANNEL_ACCESS_TOKEN = 'o8T0V8Y+K/DEvrChn7REIkaBnosq9wtc0WXYA5QePh+NMKMBpx+DkJiS5HWjdJQ6J+J8VEfDd8ZK7qdP/pbHymEzyrSJqzHGlF2hP6eBl4TQ5gztovqaKrTEbaIigYv5ByR/bTk93+Y1DVjJ2XjM2AdB04t89/1O/w1cDnyilFU='
-LINE_CHANNEL_SECRET = '380e4885058d6dc7c8f3cf7b2af37dfe'
+# 環境変数からトークン・シークレットを取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-@app.route("/webhook", methods=['POST'])
-def webhook():
+@app.route("/callback", methods=['POST'])
+def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
 
@@ -26,11 +33,9 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    reply_text = f"あなたが送ったのは「{event.message.text}」ですね！"
+    text = event.message.text
+    reply_text = f"あなたが送ったメッセージ: {text}"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
